faa5dd3685c8863c75b8dee73601a05ae702b411
Tong Lin <38441172+GermanShepherd13@users.noreply.github.com>
2022-01-20 16:58:31 -0800

    seekball

--- src/main/java/frc/robot/commands/SeekBall.java
+++ src/main/java/frc/robot/commands/SeekBall.java
@@ -6,9 +6,13 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
+import edu.wpi.first.math.controller.PIDController;
 
 public class SeekBall extends CommandBase {
   double steeringAdjust = 0.0;
+  double headingError = 0.0;
+  final double Kp = 1.0;
+  //PIDController shootMotorPID = new PIDController(1, 1, 0); //adjust
   /** Creates a new SeekBall. */
   public SeekBall() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -22,9 +26,13 @@ public class SeekBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (Robot.container.tv == 0) {
-      
+    if (Robot.container.table.getEntry("tv").getDouble(0) == 0.0) {
+      steeringAdjust = 0.3;
+    } else {
+      headingError = Robot.container.table.getEntry("tx").getDouble(0);
+      steeringAdjust = Kp * headingError;
     }
+    Robot.container.drive.drive(Robot.container.drive.getLeftMotors() +steeringAdjust, Robot.container.drive.getRightMotors() -steeringAdjust);
     
   }
 
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -16,7 +16,7 @@ public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
   public WPI_TalonFX shootMotorTalon = new WPI_TalonFX(7);
   public MotorController shootMotor = shootMotorTalon; 
-  PIDController shootMotorPID = new PIDController(1, 1, 0);
+  PIDController shootMotorPID = new PIDController(1, 1, 0); //adjust
   double setpoint;
 
   public Shooter(double setpoint) {
