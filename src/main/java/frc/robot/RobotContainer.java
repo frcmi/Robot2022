0facd19a052de2853770ff86ca38ae3c0cd4b6a7
Tong Lin <38441172+GermanShepherd13@users.noreply.github.com>
2022-01-25 16:47:44 -0800

    camera

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,11 +12,12 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import frc.robot.commands.*;
-import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 
 /**
@@ -28,7 +29,14 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
+  //Limelight and values
+  public NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+
    //Buttons and Joystick
+   
    Joystick leftJoystick = new Joystick(0);
    Joystick rightJoystick = new Joystick(1);
    JoystickButton conveyorInButton = new JoystickButton(leftJoystick, 2);
@@ -38,13 +46,6 @@ public class RobotContainer {
    JoystickButton feedButton = new JoystickButton(rightJoystick, 4);
    public JoystickButton shiftGearButton = new JoystickButton(rightJoystick, 1); //go fast
 
-   //Limelight and NetworkTables
-   public NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-   NetworkTableEntry tx = table.getEntry("tx"); //Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)
-  NetworkTableEntry ty = table.getEntry("ty"); //Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)
-  NetworkTableEntry ta = table.getEntry("ta"); //Target Area (0% of image to 100% of image)
-  public NetworkTableEntry tv = table.getEntry("tv"); //if have valid target (0 or 1)
-
   // Subsystems
   public Intake intake = new Intake();
   public DriveTrain drive = new DriveTrain();
@@ -53,6 +54,7 @@ public class RobotContainer {
   public Hanger hanger = new Hanger();
   public Feed feed = new Feed();
   public Pneumatics pneumatics = new Pneumatics();
+  public Navx navx = new Navx();
 
   //Commands
   
@@ -65,6 +67,7 @@ public class RobotContainer {
   public StartFeed startFeed = new StartFeed();
   public SolenoidReverse solenoidReverse = new SolenoidReverse();
   public SolenoidForward solenoidForward = new SolenoidForward();
+  public ShootIfStopped shootIfStopped = new ShootIfStopped();
   InstantCommand toShift = new InstantCommand(drive::shift, drive);
   RunCommand toDrive = new RunCommand(() -> drive.drive(-leftJoystick.getRawAxis(1), rightJoystick.getRawAxis(1)), drive);
   RunCommand runFlywheel = new RunCommand(() -> shooter.set(), drive);
@@ -83,11 +86,10 @@ public class RobotContainer {
   }
 
   public void initialize() {
-      // Configure the button bindings and pipeline
+      // Configure the button bindings
     configureButtonBindings();
     drive.setDefaultCommand(toDrive);
     shooter.setDefaultCommand(runFlywheel);
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0); //0 for red, 1 for blue
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/ShootIfStopped.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ShootIfStopped extends CommandBase {
+  /** Creates a new ShootIfStopped. */
+  public ShootIfStopped() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.container.shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Robot.container.navx.isMoving()) {
+      Robot.container.shooter.set(); //change
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Robot.container.shooter.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
