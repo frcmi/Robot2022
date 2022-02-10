05019b4f53ba8af02bce5333d8e8cbbb99cf6deb
jruaaa <joeyruan@zoho.com>
2022-02-09 21:00:24 -0800

    fix

---
+++ src/main/java/frc/robot/commands/ConveyorOut.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import frc.robot.subsystems.ConveyorBelt;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ConveyorOut extends CommandBase {
+  // private MotorController conveyorMotor;
+  private ConveyorBelt conveyorBelt;
+  // private double speed; 
+
+  /** Creates a new Blank. */
+  public ConveyorOut() {
+     addRequirements(Robot.container.conveyorBelt);
+      conveyorBelt = Robot.container.conveyorBelt;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() 
+  {
+    conveyorBelt.setPower(-0.50);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    conveyorBelt.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
