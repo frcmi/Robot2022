c480d63cce70ccb6b80badee5083dde87715209f
jruaaa <joeyruan@zoho.com>
2022-03-25 15:52:52 -0700

    auto stuff

--- src/main/java/frc/robot/commands/DriveOutOfTarmac.java
+++ src/main/java/frc/robot/commands/DriveOutOfTarmac.java
@@ -29,12 +29,11 @@ public class DriveOutOfTarmac extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (timer.get() < 1) { // in seconds, change this value
+    if (timer.get() < 0.5) { // in seconds, change this value
       drive.drive(-.5, -.5);
     } else {
       done = true;
     }
-   
 
   }
 
