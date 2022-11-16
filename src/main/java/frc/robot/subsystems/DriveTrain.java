bdde1e53cfb62b7de326c4f340deeb578a607c9e
eeip200 <eeip200@gmail.com>
2022-11-15 17:12:55 -0800

    Added DriveTrain
    Someone still has to implement a controller

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,29 +4,40 @@
 
 package frc.robot.subsystems;
 
-import java.sql.Struct;
-import java.util.ResourceBundle.Control;
-import java.util.function.DoubleSupplier;
+//import java.sql.Struct;
+//import java.util.ResourceBundle.Control;
+//import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Controller;
 
 public class DriveTrain extends SubsystemBase {
   Controller controller;
 
-  private static WPI_TalonFX rightBackTalonFX = 
+  private static WPI_TalonFX rightBackTalonFX = new WPI_TalonFX(Constants.rightBackPort);
+  private static WPI_TalonFX rightFrontTalonFX = new WPI_TalonFX(Constants.rightFrontPort);
+  private static WPI_TalonFX leftFrontTalonFX = new WPI_TalonFX(Constants.leftFrontPort);
+  private static WPI_TalonFX leftBackTalonFX = new WPI_TalonFX(Constants.leftBackPort);
+  private static MotorControllerGroup leftMotorGroup = new MotorControllerGroup(leftBackTalonFX, leftFrontTalonFX);
+  private static MotorControllerGroup rightMotorGroup = new MotorControllerGroup(rightBackTalonFX, rightFrontTalonFX);
+
+private static DifferentialDrive differentialDrive = new DifferentialDrive(leftMotorGroup, rightMotorGroup); 
   public DriveTrain(Controller controller) {
     this.controller = controller;
 
 
   }
-
+  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    differentialDrive.arcadeDrive(controller.getThrottleFwd(), controller.getRotation());
   }
 
   @Override
