45382a896509810a5034346d9bc86a39fd5d6ccc
Aiden <aidenychen@gmail.com>
2022-11-15 16:51:19 -0800

    Created Intake System

--- src/main/java/frc/robot/subsystems/IntakeSystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -1,7 +1,28 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSystem extends SubsystemBase{
+
+    public MotorController intakeMotorTalonFX = new WPI_TalonFX(Constants.intakeMotorID); 
+
     
+    //Sets the power of the intakeMotor
+    public void intakeMotorSetPower(double power) { 
+
+        intakeMotorTalonFX.set(power); 
+
+    }
+
+    //sets the power of the intakeMotor to 0 stopping the motor. 
+    public void intakeMotorStop() {
+
+        intakeMotorTalonFX.set(0.0); 
+
+    }
 }
